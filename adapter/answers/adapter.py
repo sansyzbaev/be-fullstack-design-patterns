3cbfc6db0b1c45ee773f61a9c6150d9d975cb6f2
sansyzbaev <sansyzbaev.islam@gmail.com>
2018-11-01 00:20:05 +0600

    Updated answers/adapter.py

--- adapter/answers/adapter.py
+++ adapter/answers/adapter.py
@@ -8,13 +8,13 @@
 
 class MediaFile:
 	
-	"""
-        Class is used to represent ordinary media file.
     """
+        Class is used to represent ordinary media file.
+    """  
 
 	def __init__(self, file):
 
-		"""
+	"""
             :param file:
             * type: str.
             * description: file path.
@@ -32,7 +32,7 @@ class MediaFile:
 
 class MediaPlayer:
 
-	"""
+    """
         Class is used to represent MediaPlayer interface, which must be implemented by child classes.
         Class has abstract method play(), which have to be implemented.
     """
@@ -61,7 +61,7 @@ class MediaPlayer:
 
 class AdvancedMediaPlayer:
 
-	"""
+    """
         Class is used to represent AdvancedMediaPlayer interface, which must be implemented by child classes.
         Class has abstract methods play_vlc() and play_mp4(), which have to be implemented.
     """
@@ -101,14 +101,14 @@ class AdvancedMediaPlayer:
 
 class MP4Player(AdvancedMediaPlayer):
 
-	"""
+    """
         Child class, which is inherited from AdvancedMediaPlayer interface, and implements play_mp4() method.
         
     """
 
 	def play_mp4(self):
 
-		"""
+	"""
             rtype: String.
             return: The current playing mediafile in 'mp4' format, otherwise raise ExceptionError with message.
             return format: "Playing "+file_name+"."+file_extension.
@@ -121,14 +121,14 @@ class MP4Player(AdvancedMediaPlayer):
 
 class VLCPlayer(AdvancedMediaPlayer):
 
-	"""
+    """
         Child class, which is inherited from AdvancedMediaPlayer interface, and implements play_vlc() method.
         
     """
 
 	def play_vlc(self):
 
-		"""
+	"""
             rtype: String.
             return: The current playing mediafile in 'vlc' format, otherwise raise ExceptionError with message.
             return format: "Playing "+file_name+"."+file_extension.
@@ -142,13 +142,13 @@ class VLCPlayer(AdvancedMediaPlayer):
 
 class MediaAdapter(MediaPlayer):
 
-	"""
+    """
         Class is used to represent MediaAdapter adapter, which is inherited from MediaPlayer interface, and implements play() method.
     """
 
 	def __init__(self, mediaFile):
 
-		"""
+	"""
             :param mediaFile:
             * type: MediaFile.
             * description: file, attached to player.
@@ -164,7 +164,7 @@ class MediaAdapter(MediaPlayer):
 
 	def play(self):
 
-		"""
+	"""
             rtype: String.
             return: 'play_vlc()' or 'play_mp4()' functions of AdvancedMediaPlayer interface.
             return format: "Playing "+file_name+"."+file_extension.
@@ -185,14 +185,14 @@ class MediaAdapter(MediaPlayer):
 
 class UniversalPlayer(MediaPlayer):
 
-	"""
+    """
         Class is used to represent UniversalPlayer interface, which is inherited from MediaPlayer interface, and implements play() method.
         
     """
 
 	def __init__(self, mediaFile):
 
-		"""
+	"""
             :param mediaFile:
             * type: MediaFile.
             * description: file, attached to player.
@@ -208,7 +208,7 @@ class UniversalPlayer(MediaPlayer):
 
 	def play(self):
 
-		"""
+	"""
             rtype: String.
             return: current playing mediafile in format 'mp3' or using adapter in format 'vlc' or 'mp4'.
             otherwise raise NotImplementedError with message 'Unsupported format'.
